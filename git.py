7e0c95a980fce29c3f857f56083628010d7e8c76
naveenkumarr2023-maker <naveenkumar.r2023@vitstudent.ac.in>
2025-12-08 12:54:11 +0530

    Add files via upload

---
+++ git.py
@@ -0,0 +1,19 @@
+n=10;
+m=20;
+sum=n+m;
+print(sum);
+
+sub=n-m;
+print("substraction is",sub);
+
+n1=int(input("Enter a number:"));
+n2=int(input("Enter 2nd numbe:"));
+sum=n1+n2;
+sub=n1-n2;
+mul=n1*n2;
+print("Additon is",sum);
+print("Substraction is",sub);
+print("Multiplicaton is",mul);
+
+name=str(input("Enter your name:"));
+print(name);
